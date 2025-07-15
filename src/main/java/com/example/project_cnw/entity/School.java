c52cc3cbecdb0477c8f022b10629dcd36d426da8
96paradise <cnw0806@naver.com>
2025-07-15 15:29:06 +0900

    feature/back-update

---
+++ src/main/java/com/example/project_cnw/entity/School.java
@@ -0,0 +1,40 @@
+package com.example.project_cnw.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "school_id")
+    private Long schoolId;
+
+    @Column(name = "school_name", nullable = false)
+    private String schoolName;
+
+    @Column(name = "school_address", nullable = false)
+    private String schoolAddress;
+
+    @Column(name = "school_contact_number", nullable = false)
+    private String schoolContactNumber;
+
+    @Column(name = "school_code", unique = true, nullable = false)
+    private Integer schoolCode;
+
+    @Column(name = "school_email", nullable = false)
+    private String schoolEmail;
+
+    @Column(name = "school_admin_username", unique = true, nullable = false)
+    private String schoolAdminUsername;
+
+    @Column(name = "school_admin_password", nullable = false)
+    private String schoolAdminPassword;
+}
+
